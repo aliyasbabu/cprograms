265b725d9d3b5b6dcb9a368f156c63a9c351581f
Deepak M S <souparnikad40@gmail.com>
2021-09-20 21:26:27 +0530

    code finished

--- problem_64.c
+++ problem_64.c
@@ -9,9 +9,23 @@ Algo:
 //solution
 #include <stdio.h>
 
+//fun declaration and intialization
+void multiply(int num,int limit){
+    int i;
+    for(i=1;i<=limit;i++){
+        printf("\n%d x %d = %d",i,num,(num*i));
+    }
+}
+
 int main(){
-    int n;
+    int limit , number;
     //read n
     printf("Enter the limit:");
+    scanf("%d", &limit);
+    printf("\nEnter the number:");
+    scanf("%d", &number);
+    //call the function
+    multiply(number,limit);
+
     return 0;
 }
