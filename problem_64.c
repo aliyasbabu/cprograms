c9c44f9df0e47a3297fb6bbf77f0672436657f07
Deepak M S <souparnikad40@gmail.com>
2021-09-20 21:08:47 +0530

    problem statement and basic syntx added

---
+++ problem_64.c
@@ -0,0 +1,15 @@
+/*
+Problem Statement:
+program to print multiplication table of a number upto a specific limit using function
+
+Algo:
+
+*/
+
+//solution
+#include <stdio.h>
+
+int main(){
+    int n;
+    return 0;
+}
